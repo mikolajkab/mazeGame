6124b867dbc247dad2e820714cbaca6372705870
Mikolaj Kabacinski <mikolaj.kabacinski@prodrive-technologies.com>
2018-02-25 14:14:13 +0100

    modifications to allow openGl work with OOP. not yet finished

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -8,8 +8,7 @@ int main(int argc, char** argv)
   CWindow Window("Maze Game");
   CMaze Maze;
   Window.Init();
-  Window.Display(Maze.getTileWidth(), Maze.getTileHeight(), &Maze.background);
-  Window.Reshape();
+  Window.Display();
   Window.Loop();
   exit(EXIT_SUCCESS);
 }
