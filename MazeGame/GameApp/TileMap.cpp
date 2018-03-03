7a92e620a18354e84a49ab8c1b97450245bec7f2
Mikolaj Kabacinski <mikolaj.kabacinski@prodrive-technologies.com>
2018-03-03 20:17:02 +0100

    cleanup

--- MazeGame/GameApp/TileMap.cpp
+++ MazeGame/GameApp/TileMap.cpp
@@ -1,54 +1,33 @@
 #include "TileMap.h"
 
-CTileMap::CTileMap()
+CTileMap::CTileMap(const std::string& tileset)
+  : tileSize(sTileWidth, sTileHeight)
 {
-}
+  sTiles = maze1;
 
-
-CTileMap::~CTileMap()
-{
-}
-
-bool CTileMap::load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
-{
   // load the tileset texture
   if (!mTexture.loadFromFile(tileset))
   {
-    return false;
+    //return false;
   }
 
   // resize the vertex array to fit the level size
   mVertexArray.setPrimitiveType(sf::Quads);
-  mVertexArray.resize(width * height * 4);
+  mVertexArray.resize(sMazeWidth * sMazeHeight * 4);
 
   // populate the vertex array, with one quad per tile
-  for (unsigned int i = 0; i < width; ++i)
-    for (unsigned int j = 0; j < height; ++j)
+  for (unsigned int col = 0; col < sMazeWidth; ++col)
+    for (unsigned int row = 0; row < sMazeHeight; ++row)
     {
-      // get the current tile number
-      int tileNumber = tiles[i + j * width];
-
-      // find its position in the tileset texture
-      int tu = tileNumber % (mTexture.getSize().x / tileSize.x);
-      int tv = tileNumber / (mTexture.getSize().x / tileSize.x);
-
       // get a pointer to the current tile's quad
-      sf::Vertex* quad = &mVertexArray[(i + j * width) * 4];
+      sf::Vertex* quad = &mVertexArray[(col + row * sMazeWidth) * 4];
 
-      // define its 4 corners
-      quad[0].position = sf::Vector2f((i * tileSize.x), (j * tileSize.y));
-      quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-      quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
-      quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
-
-      // define its 4 texture coordinates
-      quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
-      quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-      quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
-      quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+      setQuadPositions(quad, col, row);
+      setQuadCoords(quad, col, row);
     }
-
-  return true;
+}
+CTileMap::~CTileMap()
+{
 }
 
 void CTileMap::draw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -61,4 +40,29 @@ void CTileMap::draw(sf::RenderTarget& target, sf::RenderStates states) const
 
   // draw the vertex array
   target.draw(mVertexArray, states);
+}
+
+void CTileMap::setQuadPositions(sf::Vertex* quad, int col, int row)
+{
+  // define its 4 corners
+  quad[0].position = sf::Vector2f((col * tileSize.x), (row * tileSize.y));
+  quad[1].position = sf::Vector2f((col + 1) * tileSize.x, row * tileSize.y);
+  quad[2].position = sf::Vector2f((col + 1) * tileSize.x, (row + 1) * tileSize.y);
+  quad[3].position = sf::Vector2f(col * tileSize.x, (row + 1) * tileSize.y);
+}
+
+void CTileMap::setQuadCoords(sf::Vertex* quad, int col, int row)
+{
+  // get the current tile number
+  int tileNumber = sTiles[col + row * sMazeWidth];
+
+  // find its position in the tileset texture
+  int tu = tileNumber % (mTexture.getSize().x / tileSize.x);
+  int tv = tileNumber / (mTexture.getSize().x / tileSize.x);
+
+  // define its 4 texture coordinates
+  quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+  quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+  quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+  quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
 }
--- MazeGame/GameApp/TileMap.h
+++ MazeGame/GameApp/TileMap.h
@@ -2,18 +2,29 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "MazeMap.h"
+
 class CTileMap : public sf::Drawable, public sf::Transformable
 {
 public:
-  CTileMap();
+  CTileMap(const std::string& tileset);
   ~CTileMap();
-
-  bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
 
 private:
   virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+  void setQuadPositions(sf::Vertex* quad, int col, int row);
+  void setQuadCoords(sf::Vertex* quad, int col, int row);
+
+  const int* sTiles;
 
   sf::VertexArray mVertexArray;
   sf::Texture mTexture;
+  sf::Vector2u tileSize;
+
+  static constexpr int sTileWidth = 64;
+  static constexpr int sTileHeight = 64;
+
+  static constexpr int sMazeWidth = 16;
+  static constexpr int sMazeHeight = 16;
 };
 
